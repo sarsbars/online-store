cc9728450a12277e7706011b349e3a7e6df095eb
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-05-28 12:37:08 -0500

    added pages

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+function NotFound() {
+    return (
+        <section>
+            <h2>404 Not Found </h2>
+              <p className='sorry'>Sorry</p>
+              <p classname='couldnt-find'>we couldn't find that page</p>
+              <button>Back to Home</button>
+        </section>
+    );
+}
+
+export default NotFound;
