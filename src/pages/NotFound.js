c803473777a205a2d037a62c29a15be105717003
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-05-28 13:42:27 -0500

    added header link to 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,20 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import Header from '../components/Header';
 
 
 function NotFound() {
     return (
-        <section className='error-message column-direction center container flex'>
-            <h2 className='not-found'>404 Not Found </h2>
-              <p className='couldnt-find'>
-                We're sorry, the page you are looking for cannot be found.
-              </p>
-              <NavLink className='back-home' to='/'>
-                Back to Home
-            </NavLink>
-        </section>
+        <>
+            <Header />
+            <section className='error-message column-direction center container flex'>
+                <h2 className='not-found'>404 Not Found </h2>
+                <p className='couldnt-find'>
+                    We're sorry, the page you are looking for cannot be found.
+                </p>
+                <NavLink className='back-home' to='/'>
+                    Back to Home
+                </NavLink>
+            </section>
+        </>
     );
 }
 
 export default NotFound;
 
-//NEED TO ADD LINK BACK TO HOME
+//NEED TO FIX LINK BACK TO HOME
+//NEED TO FIX LINKS TO THIS PAGE???
