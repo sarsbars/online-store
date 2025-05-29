4332ba389c8bcfcbf462707c0873d0cd952e04dc
Ashedzi Solomon <solomonashe84@gmail.com>
2025-05-29 10:53:12 -0500

    Create stats components

---
+++ src/components/Stats.js
@@ -0,0 +1,24 @@
+function Stats() {
+    return (
+        <section>
+            <div className="stats-box">
+                <h3>2000+</h3>
+                <p>Happy clients</p>
+            </div>
+            <div className="stats-box">
+                <h3>300+</h3>
+                <p>Stores Worldwide</p>
+            </div>
+            <div className="stats-box">
+                <h3>1M+</h3>
+                <p>Orders shipped</p>
+            </div>
+            <div className="stats-box">
+                <h3>4.9</h3>
+                <p>Average rating</p>
+            </div>
+        </section>
+    );
+}
+
+export default Stats;
