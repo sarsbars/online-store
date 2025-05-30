b857517e5ad2b8863de347be3afae89b4a53a48d
Ashedzi Solomon <solomonashe84@gmail.com>
2025-05-30 16:46:25 -0500

    Add picture and text to small banner section

---
+++ src/components/SmallBanner.js
@@ -0,0 +1,9 @@
+function SmallBanner() {
+    return(
+        <section className="small-banner flex center">
+            <p className="small-banner-title">New here? Hurry! Get 50% off your first order.</p>
+        </section>
+    );
+}
+
+export default SmallBanner;
