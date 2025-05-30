5e78465f17e38c27d9166294deb9ddba508d7e86
Ashedzi Solomon <solomonashe84@gmail.com>
2025-05-30 15:42:55 -0500

    Add syle to the form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,31 +1,17 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ContactForm() {
-      const [userName, setUserName] = useState('');
-      const { register, handleSubmit, formState:{errors}, watch} = useForm();
-      const watchedUserName = watch("Username");
-    
-    //   useEffect(() => {
-    //     if (watchedUserName !== undefined) {
-    //       setUserName(watchedUserName.trim());
-    //     }
-    //   }, [watchedUserName]);
-      
-      // const handleChange = event => {
-        //   setUserName(event.target.value.trim());
-        // }
-        
-        // console.log(`Updated Username: ${userName}`);
+      const { register, handleSubmit, formState:{errors}} = useForm();
       const onSubmit = data => console.log('Form data sudmitted: ', data);
     return(
-         <section className="flex column-direction center">
-          <h2>Need help? Contact Us</h2>
-          <form onSubmit={handleSubmit(onSubmit)} className="contact-form">
+        <section className="flex column-direction center">
+          <h3>Need help? Contact Us</h3>
+          <form onSubmit={handleSubmit(onSubmit)} className="contact-form flex column-direction gap-20 center">
+            <label>Email</label>
             <input 
               type='text' 
               className='email-input'
-              placeholder='Email'
+              placeholder='Johnsmith@email.com'
               {...register('Email', {
               required: true,
               pattern: /^\S+@\S+$/i
@@ -33,17 +19,15 @@ function ContactForm() {
             {errors.Email && <div className="error-message"><span>*Email is required or invalid</span></div>}
             <textarea 
               type='text' 
+              rows={10}
+              cols={30}
               placeholder='Type your message'
               {...register('Meassage', {
-              required: true,
-              style: "resize: none"
+              required: true
             })}/>
             {errors.Meassage && <div className="error-message"><span>*Message is required</span></div>}
             <input type='submit' value='Submit'/>
           </form>
-          <div>
-            <p>{userName.length > 0 ? `Selected name: ${watchedUserName}` : ''}</p>
-          </div>
         </section>
     );
 }
