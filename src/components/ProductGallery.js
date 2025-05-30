200699aa23b8bf217ae67621f0e516439df5f37b
Ashedzi Solomon <solomonashe84@gmail.com>
2025-05-30 11:20:00 -0500

    Add useState and useEffect in function

--- src/components/ProductGallery.js
+++ src/components/ProductGallery.js
@@ -1,16 +1,56 @@
-function productGallery(props) {
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+function ProductGallery(props) {
+
+    const[products, setProducts] = useState([]);
+    const[error, setError] = useState('');
+    
+    const URL = 'https://fakestoreapi.com/products';
+
+
+    useEffect(() => {
+        const fetchProduct = async() => {
+            try {
+                const response = await axios.get(URL);
+                setProducts(response.data);
+                setError('');
+            } catch (error) {
+                console.log(error.message);
+                setError('Failed to fetch products');
+            }
+        };
+        fetchProduct();
+    }, []);
+
     return (
         <div>
-            {props.list.map(movie => (
-                <div key={movie.id}>
-                    <div className='poster-img'>
-                        <img src={movie.poster} className='Movie-poster'/>
-                    </div>
-                    <p>{movie.title}</p>
+            <section className="products-section">
+                <h3>Products</h3>
+                {error && <p>{error}</p>}
+                <div className='product-list'>
+                        {props.list.map(product => (
+                            <div key={product.id} className="product-card">
+                                <figure className="product-img">
+                                    <img src={image} className="product-pic" alt='product'></img>
+                                </figure>
+                                <p>{product.title}</p>
+                                <p>{product.description.substring(0, 100)}...</p>
+                                <div className="rating">
+                                    {[...Array(Math.round(product.rating.rate))].map((_, i) => (
+                                        <FontAwesomeIcon icon={faStar} className="star" key={i} />
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
                 </div>
-            ))}
+            </section>
+    
         </div>
     );
 }
 
-export default productGallery;
+export default ProductGallery;
+
